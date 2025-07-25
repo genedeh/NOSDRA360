3d1c37c711689eee991fd83db4a73bf4bd3a422b
genedeh <genesis.enedeh@gmail.com>
2025-07-25 18:35:07 +0100

    Some more icon changes

--- src/views/home/SliderSection.tsx
+++ src/views/home/SliderSection.tsx
@@ -125,7 +125,9 @@ const SliderSection = () => {
                     </Button>
 
                     <Button href="#" bgColor="bg-gray-800" hoverColor="bg-gray-900">
-                        <FiFile /> Admin Access <MdKeyboardArrowRight />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" className="lucide lucide-file-text w-5 h-5 mr-2"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z">
+                        </path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path><path d="M10 9H8"></path><path d="M16 13H8"></path><path d="M16 17H8"></path></svg> Admin Access <MdKeyboardArrowRight />
                     </Button>
 
                     <Button href="#" bgColor="bg-orange-600" hoverColor="bg-orange-700">
